72ebe935fc239773e3cbe3b43811a41fd1dd0a59
Will Gundy <willgunderson503@gmail.com>
2022-05-03 15:25:04 -0700

    added order images and names to app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [orderName, setOrderName] = useState('name');
-  const [burgerId, setBurgerId] = useState('');
+  const [orderName, setOrderName] = useState('Bob');
+  const [burgerId, setBurgerId] = useState('1');
+  const [drinkId, setDrinkId] = useState('1');
+  const [sideId, setSideId] = useState('1');
+  const [orderInstructions, setOrderInstructions] = useState('');
 
   return (
-    <h1>
-      Order for: {orderName}
-    </h1>
+    <div>
+      <section>
+        <img src={`burger-${burgerId}.png`}/>
+        <img src={`drink-${drinkId}.png`}/>
+        <img src={`side-${sideId}.png`}/>
+      </section>
+      <section>
+        <h1>
+          Order for: {orderName}
+        </h1>
+        <input onChange={(e) => {
+          setOrderName(e.target.value);
+        }}/>
+      </section>
+    </div>
   );
 }
 

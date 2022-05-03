6bdabcc50e6639fee542ee7b36d40af60b24a106
Will Gundy <willgunderson503@gmail.com>
2022-05-03 16:57:03 -0700

    destructured OrderSelectors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react';
 import OrderImages from './OrderImages';
 import OrderNameInput from './OrderNameInput';
+import OrderSelector from './OrderSelector';
 
 function App() {
   const [orderName, setOrderName] = useState('');
@@ -10,48 +11,30 @@ function App() {
   const [drinkId, setDrinkId] = useState('1');
   const [sideId, setSideId] = useState('1');
   const [orderInstructions, setOrderInstructions] = useState([]);
-  const [instructionInput, setInsturctionInput] = useState(['']);
+  const [instructionInput, setInstructionInput] = useState(['']);
+  const burgerOptions = ['Bohemian Radishy', 'Olive and Let Die', 'Hit Me With Your Best Shallot'];
+  const drinkOptions = ['Soda', 'Coffee', 'Tea'];
+  const sideOptions = ['Fries', 'Chips', 'Salad'];
 
   return (
     <div>
       <OrderImages drinkId={drinkId} burgerId={burgerId} sideId={sideId}/>
       <OrderNameInput setOrderName={setOrderName} orderName={orderName}/>
       <section>
-        <div>
-          Select a Burger:
-          <select onChange={(e) => setBurgerId(e.target.value)}>
-            <option value='1'>Bohemian Radishy</option>
-            <option value='2'>Olive and Let Die</option>
-            <option value='3'>Hit Me With Your Best Shallot</option>
-          </select>
-        </div>
-        <div>
-          Select a Side:
-          <select onChange={(e) => setSideId(e.target.value)}>
-            <option value='1'>Fries</option>
-            <option value='2'>Chips</option>
-            <option value='3'>Salad</option>
-          </select>
-        </div>
-        <div>
-          Select a Drink:
-          <select onChange={(e) => setDrinkId(e.target.value)}>
-            <option value='1'>Soda</option>
-            <option value='2'>Coffee</option>
-            <option value='3'>Tea</option>
-          </select>
-        </div>
+        <OrderSelector text={'Select a Burger:'} editor={setBurgerId} options={burgerOptions}/>
+        <OrderSelector text={'Select a Side:'} editor={setSideId} options={sideOptions}/>
+        <OrderSelector text={'Select a Drink:'} editor={setDrinkId} options={drinkOptions}/>
       </section>
       <section>
         <form onSubmit={
           (e) => {
             e.preventDefault();
             setOrderInstructions([...orderInstructions, instructionInput]);
-            setInsturctionInput('');
+            setInstructionInput('');
           }
         }>
           Add Order Instructions
-          <input value={instructionInput} onChange={(e) => setInsturctionInput(e.target.value)}/>
+          <input value={instructionInput} onChange={(e) => setInstructionInput(e.target.value)}/>
           <button>Submit</button>
         </form>
         <h2>
